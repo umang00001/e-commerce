f6e0c549b1d96d2858a1ab883aaa871cadd12447
umang00001 <131572776+umang00001@users.noreply.github.com>
2023-12-13 18:20:06 +0530

    update

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, "./client/build")))
+app.use(express.static(path.join(__dirname, "./client/build/index.html")))
 
-app.get("*", function (req, resp) {
-  resp.sendFile(path.join(__dirname, "./client/build/index.html"))
-})
+// app.get("*", function (req, resp) {
+resp.sendFile(path.join(__dirname, "./client/build/index.html"))
+// })
 //routes
 
 app.use("/api/v1/auth", authRoutes);
